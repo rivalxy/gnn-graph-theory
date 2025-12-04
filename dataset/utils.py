93f7fefa2690f1c087c79887fd9a12477a03f7ab
rivalxy <samuelvarchol@gmail.com>
2025-12-04 14:31:59 +0100

    Refactors dataset generation and graph reading
    Moves graph reading utilities to a dedicated module for better organization.
    Consolidates data processing logic within dataset generation.
    Updates dataset generation to use the new utility functions.

--- dataset/utils.py
+++ dataset/utils.py
@@ -1,3 +1,24 @@
+import networkx as nx
+from pynauty import Graph
+
+def read_graphs_from_g6(file_path: str) -> list[Graph]:
+    """
+    Reads graphs from a .g6 file and converts them to pynauty format.
+
+    :param file_path: Path to the .g6 file.
+    :returns: List of pynauty graphs with their number of nodes and edges.
+    """
+
+    graphs = nx.read_graph6(file_path)
+    pynauty_graphs = []
+    for graph in graphs:
+        num_of_nodes = int(graph.number_of_nodes())
+        pynauty_graph = Graph(num_of_nodes)
+        pynauty_graph.set_adjacency_dict(dict(graph.adjacency()))
+        pynauty_graphs.append((pynauty_graph, num_of_nodes, graph.edges()))
+    return pynauty_graphs
+
+
 def is_paut() -> bool:
     """
     Check if mapping is a partial automorphism on given graph.
--- dataset_gen.py
+++ dataset_gen.py
@@ -1,4 +1,3 @@
-import networkx as nx
 from pynauty import Graph, autgrp
 from sklearn.model_selection import train_test_split
 import random
@@ -6,27 +5,46 @@ import torch
 from torch_geometric.data import Data
 from sympy.combinatorics import Permutation, PermutationGroup
 
+from dataset.utils import *
+
 
 MAX_EXAMPLES_NUM = 30
 MAX_ATTEMPTS = 100
 
 
-def read_graphs_from_g6(file_path: str) -> list[Graph]:
-    """
-    Reads graphs from a .g6 file and converts them to pynauty format.
+def generate_positive_examples() -> list:
+    return []
 
-    :param file_path: Path to the .g6 file.
-    :returns: List of pynauty graphs with their number of nodes and edges.
-    """
 
-    graphs = nx.read_graph6(file_path)
-    pynauty_graphs = []
-    for graph in graphs:
-        num_of_nodes = int(graph.number_of_nodes())
-        pynauty_graph = Graph(num_of_nodes)
-        pynauty_graph.set_adjacency_dict(dict(graph.adjacency()))
-        pynauty_graphs.append((pynauty_graph, num_of_nodes, graph.edges()))
-    return pynauty_graphs
+def generate_negative_examples() -> list:
+    return []
+
+
+def make_pyg_data(edge_list: list[tuple], num_of_nodes: int,  mapping: dict[int, int], label: int) -> Data:
+    # 3 features: node_id, target_id, source_id
+    x = torch.full((num_of_nodes, 3), -1.0, dtype=torch.float)
+
+    # Give EVERY node its own identity
+    for node in range(num_of_nodes):
+        x[node, 0] = float(node) / num_of_nodes
+
+    # Mark mapped nodes with bidirectional info
+    for source, target in mapping.items():
+        x[source, 1] = float(target) / num_of_nodes  # target_id
+        x[target, 2] = float(source) / num_of_nodes  # source_id
+
+    if len(edge_list) > 0:
+        edges = []
+        for u, v in edge_list:
+            edges.append([u, v])
+            edges.append([v, u])
+        edge_index = torch.tensor(edges, dtype=torch.long).t().contiguous()
+    else:
+        edge_index = torch.empty((2, 0), dtype=torch.long)
+
+    y = torch.tensor([label], dtype=torch.float)
+    data = Data(x=x, edge_index=edge_index, y=y)
+    return data
 
 
 def generate_paut_dataset(graphs: list[Graph]) -> list:
@@ -69,7 +87,7 @@ def generate_paut_dataset(graphs: list[Graph]) -> list:
                 continue
             seen_positives.add(key)
             positives.append(mapping)
-            dataset.append(_make_pyg_data(
+            dataset.append(make_pyg_data(
                 edge_list, num_of_nodes, mapping, label=1))
 
         # FIXME negative examples
@@ -84,37 +102,10 @@ def generate_paut_dataset(graphs: list[Graph]) -> list:
             if key in seen_negatives:
                 continue
             seen_negatives.add(key)
-            dataset.append(_make_pyg_data(
+            dataset.append(make_pyg_data(
                 edge_list, num_of_nodes, neg_map, label=0))
 
     return dataset
-
-
-def _make_pyg_data(edge_list: list[tuple], num_of_nodes: int,  mapping: dict[int, int], label: int) -> Data:
-    # 3 features: node_id, source_id (if mapped), target_id (if mapped)
-    x = torch.full((num_of_nodes, 3), -1.0, dtype=torch.float)
-
-    # Give EVERY node its own identity
-    for node in range(num_of_nodes):
-        x[node, 0] = float(node) / num_of_nodes
-
-    # Mark mapped nodes with bidirectional info
-    for source, target in mapping.items():
-        x[source, 1] = float(target) / num_of_nodes  # target_id
-        x[target, 2] = float(source) / num_of_nodes  # source_id
-
-    if len(edge_list) > 0:
-        edges = []
-        for u, v in edge_list:
-            edges.append([u, v])
-            edges.append([v, u])
-        edge_index = torch.tensor(edges, dtype=torch.long).t().contiguous()
-    else:
-        edge_index = torch.empty((2, 0), dtype=torch.long)
-
-    y = torch.tensor([label], dtype=torch.float)
-    data = Data(x=x, edge_index=edge_index, y=y)
-    return data
 
 
 if __name__ == "__main__":
