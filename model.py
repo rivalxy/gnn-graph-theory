a17c00f7250c3c5c1a5e5339c6a804e19ce0d997
rivalxy <samuelvarchol@gmail.com>
2026-02-13 15:42:12 +0100

    Updates model and main.py accordingly

--- model.py
+++ model.py
@@ -4,31 +4,25 @@ from torch_geometric.nn import GINConv, global_add_pool
 
 
 class GIN(nn.Module):
-    def __init__(self, hidden_dim, num_layers, dropout):
+    def __init__(self, input_dim, hidden_dim, num_layers, dropout):
         super().__init__()
 
         self.dropout = dropout
         self.convs = nn.ModuleList()
 
-        self.convs.append(
-            GINConv(nn.Sequential(
-                nn.Linear(3, hidden_dim),
-                nn.BatchNorm1d(hidden_dim),
-                nn.ReLU(),
-                nn.Linear(hidden_dim, hidden_dim),
-            ))
-        )
-
         for _ in range(num_layers - 1):
             self.convs.append(
                 GINConv(nn.Sequential(
-                    nn.Linear(hidden_dim, hidden_dim),
-                    nn.BatchNorm1d(hidden_dim),
+                    nn.Linear(input_dim, 2 * hidden_dim),
+                    nn.BatchNorm1d(2 * hidden_dim),
                     nn.ReLU(),
-                    nn.Linear(hidden_dim, hidden_dim),
+                    nn.Linear(2 * hidden_dim, hidden_dim),
                 ))
             )
+            input_dim = hidden_dim
 
+        self.lin1 = nn.Linear(hidden_dim, hidden_dim)
+        self.batch_norm1 = nn.BatchNorm1d(hidden_dim)
         self.classifier = nn.Linear(hidden_dim, 1)
 
     def forward(self, data):
@@ -37,4 +31,5 @@ class GIN(nn.Module):
             x = F.relu(conv(x, edge_index))
             x = F.dropout(x, self.dropout, training=self.training)
         x = global_add_pool(x, batch)
+        x = F.relu(self.batch_norm1(self.lin1(x)))
         return self.classifier(x).view(-1)
