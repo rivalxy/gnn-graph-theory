db75379576c3260a8150b8ed15aa3e08a062a334
rivalxy <samuelvarchol@gmail.com>
2026-03-16 17:24:01 +0100

    refactor(model): change output dimension in GIN model

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ from torch_geometric.nn import GINConv, GPSConv, global_add_pool
 
 
 class GIN(nn.Module):
-    def __init__(self, input_dim, hidden_dim, num_layers, dropout, num_classes=1):
+    def __init__(self, input_dim, hidden_dim, num_layers, dropout):
         super().__init__()
         self.dropout = dropout
         self.convs = nn.ModuleList()
@@ -25,7 +25,7 @@ class GIN(nn.Module):
             self.batch_norms.append(nn.BatchNorm1d(hidden_dim))
 
         self.lin1 = nn.Linear(hidden_dim, hidden_dim)
-        self.classifier = nn.Linear(hidden_dim, num_classes)
+        self.classifier = nn.Linear(hidden_dim, 1)
 
     def forward(self, data):
         x, edge_index, batch = data.x, data.edge_index, data.batch
